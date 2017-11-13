04987354b804970b5284c3a4761e90c8e61d02b2
Anselmos <anselmos@users.noreply.github.com>
2017-11-13 12:17:41 +0100

    Adds better run and initial version of keepass installer.

---
+++ build.sh
@@ -0,0 +1,3 @@
+IMAGE_NAME=anselmos/keepassx:2.0.alpha2
+
+docker build -t $IMAGE_NAME .
--- run.sh
+++
@@ -1,5 +0,0 @@
-docker run -ti --rm \
-    -e DISPLAY=unix$DISPLAY \
-    --privileged \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    anselmos/keepassx
